7ec776e429f874023cfa5c6abb98be39e202aaa8
FelipeFranke5 <frankefelipee@gmail.com>
2025-05-08 13:08:37 -0300

    feat: include main repo into EC2 instance

--- init.sh
+++ init.sh
@@ -20,6 +20,16 @@ ssh -i "$SSH_KEY_PATH" "$EC2_USER@$EC2_HOST" << EOF
         apt install -y build-essential python3-dev
         apt install -y libssl-dev libffi-dev
         echo "Finished installing Python and its dependencies"
+        DIR="braspag-automation-kafka"
+        echo "Checking if main directory exists"
+        if [ -d "$DIR" ]; then
+            echo "Directory '$DIR' exists"
+            cd braspag-automation-kafka
+        else
+            echo "Directory '$DIR' does not exist"
+            git clone https://github.com/FelipeFranke5/braspag-automation-kafka.git
+            cd braspag-automation-kafka
+        fi
         echo "Initilizing main script"
         chmod +x ./init_inside_ssh.sh
         ./init_inside_ssh.sh
